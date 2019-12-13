629d58596c67a513a9c9e55ba803171ac8168166
Madjid SAHKI <b_atmans@hotmail.fr>
2019-12-13 13:56:54 +0100

    parse comment + post

--- src/app/services/MessageParser.ts
+++ src/app/services/MessageParser.ts
@@ -1,5 +1,6 @@
 import {
     Post,
+    Comment,
     PostContent,
     YoutubePostContent,
     PicturePostContent,
@@ -15,23 +16,25 @@ const youtube = "https://youtu.be/";
 export class MessageParser {
 
     parse(post: Post): PostContent<any> {
-        const pictureRegex = /http[s]?:\/\/.+\.(jpeg|png|jpg|gif)/gmi;
+        const pictureRegex = /^http[^ \!@\$\^&\(\)\+\=]+(\.png|\.jpeg|\.gif|\.jpg)$/;
         const pictureMatche = pictureRegex.exec(post.message);
         if (pictureMatche) {
+            post.message = '';
             return new PicturePostContent(post.message);
         }
 
-        // const videoRegex = / /gmi;  // TODO
-        // const videoMatche = videoRegex.exec(post.message);
-        // if(videoMatche) {
-        //     return new VideoPostContent(post.message);
-        // }
+        const videoRegex = /^http[^ \!@\$\^&\(\)\+\=]+(\.mp4)$/;
+        const videoMatche = videoRegex.exec(post.message);
+        if(videoMatche) {
+            post.message = '';
+            return new VideoPostContent(post.message);
+        }
         // retourner une instance de VideoPostContent si match
 
         const youtubeRegex = /(http[s]?:\/\/)?www\.(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\/?\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/gmi;
         const youtubeMatche = youtubeRegex.exec(post.message);
         if (youtubeMatche) {
-            console.log(post.message);
+            post.message = '';
             return new YoutubePostContent(youtubeMatche[2]);
         }
         // retourner une instance de YoutubePostContent si match
